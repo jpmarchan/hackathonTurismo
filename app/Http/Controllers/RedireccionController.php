61799ffc6f3c6a61d8ec35d20cfaf2cd838459c0
Lum3xxx <jmarchanz@autonoma.edu.pe>
2019-11-30 20:42:31 -0500

    diseno

---
+++ app/Http/Controllers/RedireccionController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class redireccionController extends Controller
+{
+    public function redirecion()
+    {
+
+            return view('segundoPage');
+
+    }
+}
